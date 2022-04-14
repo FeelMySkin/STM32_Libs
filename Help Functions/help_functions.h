dc5176ddab6d04f2e664c00282a155cb823dbbd7
Phil <zetsuboulevel@gmail.com>
2022-04-14 19:41:00 +0300

    Small changes in CompareWildStrings

--- Help Functions/help_functions.h
+++ Help Functions/help_functions.h
@@ -3,7 +3,7 @@
 
 #include "defines.h"
 
-bool CompareWildStrings(const char* wild, char* string, char wild_card='*');
+bool CompareWildStrings(const char* wild, const char* string, char wild_card='*');
 bool ComparePartStrings(const char* str1, const char* str2);
 uint16_t GetLengthToSymbol(const char* str,char symbol);
 uint16_t CopyString(char* str_to,const char* str_from,uint16_t length=65000);
