ea50cfe089827c569ee4c795d604325208ea6cd7
Phil <zetsuboulevel@gmail.com>
2022-08-11 14:47:37 +0300

    added ll crc include

--- F4/Help Functions/periph_functions.h
+++ F4/Help Functions/periph_functions.h
@@ -18,6 +18,7 @@
 #include "stm32f4xx_ll_bus.h"
 #include "stm32f4xx_ll_pwr.h"
 #include "stm32f4xx_ll_iwdg.h"
+#include "stm32f4xx_ll_crc.h"
 
 void EnableExtiIRQn(uint32_t exti_line,uint8_t priority);
 void EnableTimIRQn(TIM_TypeDef* tim,uint8_t priority);
